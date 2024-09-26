7bc229ea68ddb3643ec4ea24dc03cec4fd84374e
JJranji <94059043+JJranji@users.noreply.github.com>
2024-09-26 18:09:57 +0200

    Initial Commit

---
+++ TabbarApp_SwiftUI/ContentView.swift
@@ -0,0 +1,69 @@
+//
+//  ContentView.swift
+//  TabbarApp_SwiftUI
+//
+//  Created by JJ on 26/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }
+
+            SearchView()
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Search")
+                }
+
+            SettingsView()
+                .tabItem {
+                    Image(systemName: "gearshape.fill")
+                    Text("Settings")
+                }
+        }
+    }
+}
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Text("Home")
+                .font(.largeTitle)
+                .padding()
+        }
+    }
+}
+
+struct SearchView: View {
+    var body: some View {
+        VStack {
+            Text("Search")
+                .font(.largeTitle)
+                .padding()
+        }
+    }
+}
+
+struct SettingsView: View {
+    var body: some View {
+        VStack {
+            Text("Settings")
+                .font(.largeTitle)
+                .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ TabbarApp_SwiftUI/TabbarApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabbarApp_SwiftUIApp.swift
+//  TabbarApp_SwiftUI
+//
+//  Created by JJ on 26/09/24.
+//
+
+import SwiftUI
+
+@main
+struct TabbarApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
